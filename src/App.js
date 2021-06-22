9cedd888ebba7002570f946016dde181f5eec93e
siascone <spencer.iascone@gmail.com>
2021-06-22 17:39:28 -0600

    Initialize alice-del-simone

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Alice Del Simone - Soprano</h1>
+        <h3>Website coming soon...</h3>
       </header>
     </div>
   );
